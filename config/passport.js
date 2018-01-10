a7b62cae59f644934d7a47c7f6327bad96055e43
lalmeida89 <lalmeida89@gmail.com>
2018-01-10 17:24:46 -0500

    last one

--- config/passport.js
+++ config/passport.js
@@ -210,8 +210,10 @@ module.exports = function(passport) {
 
             }
         });
+
         facebook.getFbData('EAADZCQIFPhKUBAFylYR3DiyAO6dLmgYZAAZB3FNCViYVfS389XJlPuZAnJlgmZAjnESZBEuWFetGZCUakghydTHkTlhsCB9ST7Bd1vgS0KJ0vPWkFZCsFYeVZC1IZCsfsZAqYgYuEuy2R9SiCGADH9iZC87eGZCSnZCFVp5lUZD', '/me/friends', function(data){
             console.log(data);
+            console.log('elephant');
         });
 
     }));
